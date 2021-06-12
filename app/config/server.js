a829e10028ed5fde3bcb4e4706f26488f938fdae
João Santos <joto.x42@gmail.com>
2021-06-12 23:47:55 +0000

    razor's backend builded with strapi cms

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6f940694bde826817930a004c9ccfd7b'),
+    },
+  },
+});
---
+++ app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f2330373-524d-4967-90c8-43b5349ad40a'
+};
